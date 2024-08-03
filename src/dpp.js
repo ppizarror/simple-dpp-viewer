35675d199fed6f97e9f66bbfa619098055535dbf
Pablo Pizarro R. <pablo@ppizarror.com>
2024-08-02 23:59:50 -0400

    Finishes base dpp dataset

--- src/dpp.js
+++ src/dpp.js
@@ -3,37 +3,69 @@
  */
 
 import * as THREE from 'three';
+import {MD5} from 'https://cdn.skypack.dev/crypto-js';
+import {initViewer} from "./viewer.js";
+
+// Util: Apply hash to md5 string
+export function md5Hash(string) {
+    return MD5(string).toString();
+}
 
 /**
  * Creates a DPP object.
  *
  * @param {string} id - DPP id
  * @param {string} material - DPP materiality
+ * @param {string} label - DPP label
  * @param {number} condition - Material condition (0-100)
  * @param {number} reusability - Material reusability (0-100)
- * @param {THREE.MeshStandardMaterial} viewerTextObj
+ * @param {string} dimensions - Dimensions
  * @constructor
  */
-function DPP(id, material, condition, reusability, viewerTextObj) {
+function DPP(id, material, label, condition, reusability, dimensions) {
+    let self = this;
     this.id = id;
     this.material = material;
+    this.label = label;
     this.condition = condition;
     this.reusability = reusability;
-    this.viewerTextObj = viewerTextObj;
+    this.dimensions = dimensions;
+    /** @type {THREE.MeshStandardMaterial} */ this.viewerTextObj = null;
+
+    this.bindMaterial = function (material) {
+        self.viewerTextObj = material;
+        self.viewerTextObj.color = new THREE.Color(0xffffff);
+        self.viewerTextObj.emissive = new THREE.Color(0xff0000); // Set emissive color
+        self.viewerTextObj.emissiveIntensity = 0;
+    };
 }
 
 /**
  * Defines a new Database.
  *
  * @param {Array<DPP>} objects - List of objects that hold the database
+ * @param {string} model_name - Model name
  * @param {string} model_path - Model path of the BIM
  */
-function DataBase(objects, model_path) {
+function DataBase(objects, model_name, model_path) {
+    let self = this;
     this.objects = objects;
+    this.model_name = model_name;
     this.model_path = model_path;
 
-    // Initializes rendering
-    this.initRender = function () {
+    /** @param {THREE.MeshStandardMaterial} texture */
+    this.bindMaterial = function (texture) {
+        const texture_id = md5Hash(texture['name']);
+        self.objects.forEach((obj) => {
+            if (obj.id === texture_id) obj.bindMaterial(texture);
+        });
+    };
 
+    // Initializes the viewer
+    this.initViewer = function () {
+        document.title = `DPP Viewer: ${self.model_name}`;
+        initViewer(self);
     };
 }
+
+export {DataBase, DPP}
--- src/viewer.js
+++ src/viewer.js
@@ -2,29 +2,6 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 
-// Init
-const renderer = new THREE.WebGLRenderer({antialias: true});
-const scene = new THREE.Scene();
-
-// Add lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 2); // Soft white light
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-directionalLight.position.set(5, 10, 7.5);
-scene.add(directionalLight);
-
-const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-pointLight.position.set(10, 10, 10);
-scene.add(pointLight);
-
-// Create camera
-const camera = new THREE.PerspectiveCamera(70, 1, 0.01, 100);
-camera.position.z = 1;
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
 // Utils: Scale the model to a factor of 1
 function model_scale(model) {
     const box = new THREE.Box3().setFromObject(model);
@@ -40,62 +17,93 @@ function model_scale(model) {
     model.scale.set(scaleFactor, scaleFactor, scaleFactor);
 }
 
-// Load GLTF model
-const loader = new GLTFLoader();
-loader.load(
-    'scene/home.glb',
-    function (gltf) {
-        model_scale(gltf.scene);
-        gltf.scene.traverse((child) => {
-            if (child.isMesh) {
-                /** @type {THREE.MeshStandardMaterial} */ const material = child.material;
-                if (material && material.map) {
-                    if (material.name === 'snapshot01') material.opacity = 0;
-                    console.warn(material);
+/**
+ * Initializes the viewer.
+ *
+ * @param {DataBase} database - Model database
+ */
+function initViewer(database) {
+    // Init
+    const renderer = new THREE.WebGLRenderer({antialias: true});
+    const scene = new THREE.Scene();
+
+    // Add lighting
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2); // Soft white light
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(5, 10, 7.5);
+    scene.add(directionalLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+    pointLight.position.set(10, 10, 10);
+    scene.add(pointLight);
+
+    // Create camera
+    const camera = new THREE.PerspectiveCamera(70, 1, 0.01, 100);
+    camera.position.z = 1;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+
+    // Load GLTF model
+    const loader = new GLTFLoader();
+    loader.load(
+        database.model_path,
+        function (gltf) {
+            model_scale(gltf.scene);
+            gltf.scene.traverse((child) => {
+                if (child.isMesh) {
+                    const material = child.material;
+                    if (material && material.map) {
+                        database.bindMaterial(material);
+                    }
                 }
-            }
-        });
-        // Adjust camera and controls to frame the loaded model
-        const box = new THREE.Box3().setFromObject(gltf.scene);
-        const center = box.getCenter(new THREE.Vector3());
-        const size = box.getSize(new THREE.Vector3());
+            });
+            // Adjust camera and controls to frame the loaded model
+            const box = new THREE.Box3().setFromObject(gltf.scene);
+            const center = box.getCenter(new THREE.Vector3());
+            const size = box.getSize(new THREE.Vector3());
 
-        const maxDim = Math.max(size.x, size.y, size.z);
-        const fov = camera.fov * (Math.PI / 180);
-        let cameraZ = Math.abs(maxDim / 4 * Math.tan(fov * 2));
+            const maxDim = Math.max(size.x, size.y, size.z);
+            const fov = camera.fov * (Math.PI / 180);
+            let cameraZ = Math.abs(maxDim / 4 * Math.tan(fov * 2));
 
-        cameraZ *= 3; // Zoom out a little so that the model is fully visible
+            cameraZ *= 3; // Zoom out a little so that the model is fully visible
 
-        camera.position.z = center.z + 1.25 * cameraZ;
-        camera.position.y = center.y + 0.5 * cameraZ;
-        camera.position.x = center.x + -cameraZ;
-        camera.lookAt(center);
+            camera.position.z = center.z + 1.25 * cameraZ;
+            camera.position.y = center.y + 0.5 * cameraZ;
+            camera.position.x = center.x + -cameraZ;
+            camera.lookAt(center);
 
-        controls.target = center;
-        controls.update();
-        scene.add(gltf.scene);
-    },
-    undefined,
-    function (error) {
-        console.error('Error loading GLTF model', error);
+            controls.target = center;
+            controls.update();
+            scene.add(gltf.scene);
+        },
+        undefined,
+        function (error) {
+            console.error('Error loading GLTF model', error);
+        }
+    );
+
+    // Resize handler
+    function resize_handler() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
     }
-);
 
-// Resize handler
-function resize_handler() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    window.addEventListener('resize', resize_handler);
+    resize_handler();
+
+    // Animation loop
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
+
+    function animate() {
+        controls.update();
+        renderer.render(scene, camera);
+    }
 }
 
-window.addEventListener('resize', resize_handler);
-resize_handler();
-
-// Animation loop
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-function animate() {
-    controls.update();
-    renderer.render(scene, camera);
-}
+export {initViewer}
